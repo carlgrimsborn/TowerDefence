28a3e0570a5f70366cda435a5044dd4121d857ae
Sebastian Nilsson <sebastian.nilsson@live.com>
2018-04-17 09:10:35 +0200

    almost done with tutorial 5?

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class Turret : MonoBehaviour {
 
 	private Transform target;
+
+	[Header("Attributes")]
+
 	public float range = 15f;
+	public float fireRate = 1f;
+	private float fireCountdown = 0f;
+
+	[Header("Unity Setup Fields")]
 
 	public string enemyTag = "Enemy";
 
 	public Transform partToRotate;
 	public float turnSpeed = 5f;
+
+	public GameObject bulletPrefab;
+	public Transform firePoint;
 
 	// Use this for initialization
 	void Start ()
@@ -55,6 +65,21 @@ public class Turret : MonoBehaviour {
 		Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
 		partToRotate.rotation = Quaternion.Euler (0f, rotation.y, 0f);
 
+		if (fireCountdown <= 0f) {
+			Shoot ();
+			fireCountdown = 1f / fireRate;
+		}
+
+		fireCountdown -= Time.deltaTime;
+	}
+
+	void Shoot () {
+		GameObject bulletGo = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+		Bullet bullet = bulletGo.GetComponent<Bullet> ();
+
+		if (bullet != null) {
+			bullet.Seek (target);
+		}
 	}
 
 	/// <summary>
