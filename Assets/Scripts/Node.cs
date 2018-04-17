ca04a792973ad28a8aafeca276d5b419ee54f42d
Sebastian Nilsson <sebastian.nilsson@live.com>
2018-04-17 17:58:45 +0200

    Added enemy shooting and effects 5:48 ep6

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour {
+
+	public Color hoverColor;
+
+	private Renderer rend;
+	private Color startColor;
+
+	void Start () {
+		rend = GetComponent<Renderer>();
+		startColor = rend.material.color;
+	}
+
+	void onMouseEnter () {
+		rend.material.color = hoverColor;
+	}
+
+	void onMouseExit() {
+		rend.material.color = startColor;
+	}
+	void Update() {
+		
+		Touch myTouch = Input.GetTouch(0);
+		Touch[] myTouches = Input.touches;
+		for (int i = 0; i < Input.touchCount; i++) {
+			Debug.Log ("Hej!");
+		}
+	}
+}
