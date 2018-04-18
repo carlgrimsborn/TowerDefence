7f9f8abda79c2f07b32a8a926204e9a7a19f36f4
Carl Grimsborn <carlgrimsborn@hotmail.com>
2018-04-18 17:42:55 +0200

    tutorial 6 complete turret can be placed, Update() method in Node.cs is causing some bugs and is therefore commented out

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -4,28 +4,46 @@ using System.Collections;
 public class Node : MonoBehaviour {
 
 	public Color hoverColor;
+	public Vector3 positionOffset;
+
+	private GameObject turret;
 
 	private Renderer rend;
 	private Color startColor;
 
-	void Start () {
+	void Start ()
+	{
 		rend = GetComponent<Renderer>();
 		startColor = rend.material.color;
 	}
 
-	void onMouseEnter () {
+	void OnMouseDown ()
+	{
+		if (turret != null)
+		{
+			Debug.Log("Can't build there! - TODO: Display on screen");
+			return;
+		}
+
+		GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+		turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+	}
+
+	void OnMouseEnter ()
+	{
 		rend.material.color = hoverColor;
 	}
 
-	void onMouseExit() {
+	void OnMouseExit()
+	{
 		rend.material.color = startColor;
 	}
-	void Update() {
-		
-		Touch myTouch = Input.GetTouch(0);
-		Touch[] myTouches = Input.touches;
-		for (int i = 0; i < Input.touchCount; i++) {
-			Debug.Log ("Hej!");
-		}
-	}
+	// void Update() {
+
+	// 	Touch myTouch = Input.GetTouch(0);
+	// 	Touch[] myTouches = Input.touches;
+	// 	for (int i = 0; i < Input.touchCount; i++) {
+	// 		Debug.Log ("Hej!");
+	// 	}
+	// }
 }
