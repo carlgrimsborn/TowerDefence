24073e6613c6c05dacf39efb16cca3563d3df19b
Sebastian Nilsson <sebastian.nilsson@live.com>
2018-04-20 15:39:14 +0200

    next tutorial is 10

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -17,16 +17,17 @@ public class BuildManager : MonoBehaviour {
 	}
 
 	public GameObject standardTurretPrefab;
-
-	void Start ()
-	{
-		turretToBuild = standardTurretPrefab;
-	}
+	public GameObject missileLauncherPrefab;
 
 	private GameObject turretToBuild;
 
 	public GameObject GetTurretToBuild ()
 	{
 		return turretToBuild;
+	}
+
+	public void SetTurretToBuild (GameObject turret)
+	{
+		turretToBuild = turret;
 	}
 }
---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Shop : MonoBehaviour {
+
+	BuildManager buildManager;
+
+	void Start () {
+
+		buildManager = BuildManager.instance;
+
+	}
+
+	public void PurchaseStandardTurret () {
+
+		Debug.Log("standardTurret");
+		buildManager.SetTurretToBuild(buildManager.standardTurretPrefab);
+
+	}
+
+	public void PurchaseMissileLauncher () {
+
+		Debug.Log("missilskjutare vald");
+		buildManager.SetTurretToBuild(buildManager.missileLauncherPrefab);
+
+	}
+}
